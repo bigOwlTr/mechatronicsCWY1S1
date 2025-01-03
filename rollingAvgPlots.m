2b0100f342cc88edb5a13b03c0f7824b540a88f9
Colin Peden <colin.p.peden@gmail.com>
2025-01-03 11:28:44 +0000

    plot ylim change

--- rollingAvgPlots.m
+++ rollingAvgPlots.m
@@ -1,22 +1,31 @@
-clear;close all;clc;        %Clearing the workspace and any windows
+% Clear workspace and close figures
+clear; close all; clc;
 
-window5 = load("rollingAvgData/1m5windowUncalib.mat");      %load data
+% Load data
+window5 = load("rollingAvgData/1m5windowUncalib.mat");
 window10 = load("rollingAvgData/1m10windowUncalib.mat");
 window25 = load("rollingAvgData/1m25windowUncalib.mat");
 window50 = load("rollingAvgData/1m50windowUncalib.mat");
 
-figure;         %open the figure
+% Create figure with specified size
+figure('Units', 'inches', 'Position', [0, 0, 6, 4]);
+
+% Set font size for axes
+set(gca, 'FontSize', 12);
+
+% Plot rolling averages with increased line width
 hold on;
-
-% Plot rolling averages
-plot(window5.distanceTable.Time,window5.distanceTable.RollingAvg,'b','DisplayName','5-window');
-plot(window10.distanceTable.Time,window10.distanceTable.RollingAvg,'g','DisplayName','10-window');
-plot(window25.distanceTable.Time,window25.distanceTable.RollingAvg,'m','DisplayName','25-window');
-plot(window50.distanceTable.Time,window50.distanceTable.RollingAvg,'r','DisplayName','50-window');
-
-xlabel('Time [s]');         %axis labels
-ylabel('Distance [m]');
-legend('show');             %legend
-grid on;                    %show grid lines
-
+plot(window5.distanceTable.Time, window5.distanceTable.RollingAvg, 'b', 'LineWidth', 1.5, 'DisplayName', '5-window');
+plot(window10.distanceTable.Time, window10.distanceTable.RollingAvg, 'g', 'LineWidth', 1.5, 'DisplayName', '10-window');
+plot(window25.distanceTable.Time, window25.distanceTable.RollingAvg, 'm', 'LineWidth', 1.5, 'DisplayName', '25-window');
+plot(window50.distanceTable.Time, window50.distanceTable.RollingAvg, 'r', 'LineWidth', 1.5, 'DisplayName', '50-window');
 hold off;
+
+% Label axes with increased font size
+xlabel('Time [s]', 'FontSize', 12);
+ylabel('Distance [m]', 'FontSize', 12);
+
+% Add legend with increased font size
+legend('show', 'FontSize', 12);
+
+
