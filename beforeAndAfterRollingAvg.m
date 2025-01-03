77a5acf449ffb1e5074ad768b44d7e752eaeeff3
Colin Peden <colin.p.peden@gmail.com>
2025-01-03 11:43:28 +0000

    script that plots with and without rolling avg

---
+++ beforeAndAfterRollingAvg.m
@@ -0,0 +1,29 @@
+%clear workspace and close figures
+clear; close all; clc;
+
+%load data
+data = load("1mNoCalib.mat");
+
+
+figure(1);
+set(gcf, 'Position', [100, 100, 700, 700]);  %set figure size 
+set(gca, 'FontSize', 24);
+hold on;
+plot(data.distanceTable.Time, data.distanceTable.Distance, 'k', 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Distance (m)');
+ylim([0.975 1.025])
+
+hold off;
+
+figure(2);
+set(gcf, 'Position', [100, 100, 700, 700]);  %set figure size 
+set(gca, 'FontSize', 24);
+hold on;
+plot(data.distanceTable.Time, data.distanceTable.RollingAvg, 'k', 'LineWidth', 2);
+
+ylim([0.975 1.025])
+xlabel('Time (s)');
+ylabel('Distance (m)');
+hold off;
+
