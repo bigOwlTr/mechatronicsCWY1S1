caf84b314f639c2daedf0f0803b8dd00b287f7a0
Colin Peden <colin.p.peden@gmail.com>
2025-01-03 11:31:09 +0000

    changed comments and double specified font size

--- rollingAvgPlots.m
+++ rollingAvgPlots.m
@@ -1,19 +1,19 @@
-% Clear workspace and close figures
+%clear workspace and close figures
 clear; close all; clc;
 
-% Load data
+%load data
 window5 = load("rollingAvgData/1m5windowUncalib.mat");
 window10 = load("rollingAvgData/1m10windowUncalib.mat");
 window25 = load("rollingAvgData/1m25windowUncalib.mat");
 window50 = load("rollingAvgData/1m50windowUncalib.mat");
 
-% Create figure with specified size
+%create figure with specified size
 figure('Units', 'inches', 'Position', [0, 0, 6, 4]);
 
-% Set font size for axes
+%set font size
 set(gca, 'FontSize', 12);
 
-% Plot rolling averages with increased line width
+%plot rolling averages
 hold on;
 plot(window5.distanceTable.Time, window5.distanceTable.RollingAvg, 'b', 'LineWidth', 1.5, 'DisplayName', '5-window');
 plot(window10.distanceTable.Time, window10.distanceTable.RollingAvg, 'g', 'LineWidth', 1.5, 'DisplayName', '10-window');
@@ -21,11 +21,11 @@ plot(window25.distanceTable.Time, window25.distanceTable.RollingAvg, 'm', 'LineW
 plot(window50.distanceTable.Time, window50.distanceTable.RollingAvg, 'r', 'LineWidth', 1.5, 'DisplayName', '50-window');
 hold off;
 
-% Label axes with increased font size
-xlabel('Time [s]', 'FontSize', 12);
-ylabel('Distance [m]', 'FontSize', 12);
+%label axes
+xlabel('Time [s]');
+ylabel('Distance [m]');
 
-% Add legend with increased font size
-legend('show', 'FontSize', 12);
+%add legend
+legend('show');
 
 
