e4cd469d705b1ec130c6226b85c1bc294856fd04
Colin Peden <colin.p.peden@gmail.com>
2025-01-03 11:41:42 +0000

    script that plots mesurement with and without rolling avg of 25

--- rollingAvgPlots.m
+++ rollingAvgPlots.m
@@ -15,10 +15,10 @@ set(gca, 'FontSize', 12);
 
 %plot rolling averages
 hold on;
-plot(window5.distanceTable.Time, window5.distanceTable.RollingAvg, 'b', 'LineWidth', 1.5, 'DisplayName', '5-window');
-plot(window10.distanceTable.Time, window10.distanceTable.RollingAvg, 'g', 'LineWidth', 1.5, 'DisplayName', '10-window');
-plot(window25.distanceTable.Time, window25.distanceTable.RollingAvg, 'm', 'LineWidth', 1.5, 'DisplayName', '25-window');
-plot(window50.distanceTable.Time, window50.distanceTable.RollingAvg, 'r', 'LineWidth', 1.5, 'DisplayName', '50-window');
+plot(window5.distanceTable.Time, window5.distanceTable.RollingAvg, 'b', 'LineWidth', 1, 'DisplayName', '5-window');
+plot(window10.distanceTable.Time, window10.distanceTable.RollingAvg, 'g', 'LineWidth', 1, 'DisplayName', '10-window');
+plot(window25.distanceTable.Time, window25.distanceTable.RollingAvg, 'm', 'LineWidth', 1, 'DisplayName', '25-window');
+plot(window50.distanceTable.Time, window50.distanceTable.RollingAvg, 'r', 'LineWidth', 1, 'DisplayName', '50-window');
 hold off;
 
 %label axes
